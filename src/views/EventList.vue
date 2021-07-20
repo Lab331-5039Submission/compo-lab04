93830ca357ff82f2f31e1969190536b700f1af6e
Oralita <oat431@gmail.com>
2021-07-21 05:08:00 +0700

    [🍉 finish] : number 2

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,7 +1,31 @@
 <template>
   <h1>Events For Good</h1>
+  <select v-model.number="size">
+    <option v-for="num in index" :key="num">
+      {{ num }}
+    </option>
+  </select>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <div class="pagination">
+      <router-link
+        id="page-prev"
+        :to="{ name: 'EventList', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+      >
+        Prev Page
+      </router-link>
+
+      <router-link
+        id="page-next"
+        :to="{ name: 'EventList', query: { page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPage"
+      >
+        Next Page
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -9,25 +33,45 @@
 // @ is an alias to /src
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
+import { watchEffect } from '@vue/runtime-core'
 // import axios from 'axios'
 export default {
   name: 'EventList',
+  props: {
+    page: {
+      type: Number,
+      required: true
+    }
+  },
   components: {
     EventCard // register it as a child component
   },
   data() {
     return {
-      events: null
+      events: null,
+      totalEvents: 0,
+      index: null,
+      size: 1
     }
   },
   created() {
-    EventService.getEvents()
-      .then((response) => {
-        this.events = response.data
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+    watchEffect(() => {
+      EventService.getEvents(this.size, this.page)
+        .then((response) => {
+          this.events = response.data
+          this.totalEvents = response.headers['x-total-count']
+          this.index = Array.from({ length: this.totalEvents }, (_, i) => i + 1)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    })
+  },
+  computed: {
+    hasNextPage() {
+      let totalPage = Math.ceil(this.totalEvents / this.size)
+      return this.page < totalPage
+    }
   }
 }
 </script>
@@ -36,5 +80,20 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+#page-prev {
+  text-align: left;
+}
+#page-next {
+  text-align: right;
 }
 </style>
